ef27a518bdf4ab96478cb8979372427c3b115e56
a.shahzad <a.shahzad@id-ware.com>
2019-10-25 13:12:16 +0500

    update talk, ignore mapping for speaker and camp,add validation to talkmodel

--- Controllers/TalksController.cs
+++ Controllers/TalksController.cs
@@ -45,5 +45,113 @@ namespace CoreCodeCamp.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, $"Database Failure {ex}");
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Get( string moniker, int id)
+        {
+            try
+            {
+                var result = await _repository.GetTalkByMonikerAsync(moniker, id);
+                if (result == null)
+                {
+                    return NotFound("No talks found");
+                }
+
+                TalkModel model = _mapper.Map<TalkModel>(result);
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Database Failure {ex}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TalkModel>> Post( string moniker, TalkModel model)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+                // if there is no camp with moniker return
+                if (camp == null) return BadRequest("Camp does not exist");
+
+                // map the posted talk to Talk model
+                var talk = _mapper.Map<Talk>(model);
+                // assign camp that we get above to the talk.
+                talk.Camp = camp;
+
+                // check if speaker id exist in the post data
+                if (model.Speaker == null) return BadRequest("Speaker ID is required");
+                var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                // return if speaker with speaker id does not exist
+                if (speaker == null) return BadRequest("Speaker could not be found");
+                // add speaker to the talk
+                talk.Speaker = speaker;
+
+                // add talk with camp associated
+                _repository.Add(talk);
+
+                if ( await _repository.SaveChangesAsync() )
+                {
+                    // get location to set in response header
+                    var location = _linkGenerator.GetPathByAction(HttpContext,
+                        "Get",
+                        values: new { moniker, id = talk.TalkId});
+                    // convert talk back to TalkModel
+                    TalkModel talkModel = _mapper.Map<TalkModel>(talk);
+                    return Created(location, talkModel);
+                } else
+                {
+                    return BadRequest("Failed to save new talk");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Database Failure {ex}");
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Put( string moniker, int id, TalkModel model)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id, true); // true to get speakers
+
+                if (talk == null) return NotFound("Could not find the talk");
+
+                //
+                // 
+                /**
+                 * similar to : model.Title = talk.Title; 
+                 * map everything in the model to everything in the talk : not needed, modified CampsProfile
+                 * speaker and camp is ignored in the CampProfile
+                 */
+                _mapper.Map(model, talk);
+
+                // attaching speaker manuall
+                if ( model.Speaker != null )
+                {
+                    var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                    if ( speaker != null )
+                    {
+                        talk.Speaker = speaker;
+                    }
+                }
+
+                if ( await _repository.SaveChangesAsync() )
+                {
+                    var modal = _mapper.Map<TalkModel>(talk);
+                    return Ok(modal);
+                } else
+                {
+                    return BadRequest("Failed to update talk");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Database Failure {ex.Message}");
+            }
+        }
     }
 }
--- Data/CampProfile.cs
+++ Data/CampProfile.cs
@@ -11,16 +11,26 @@ namespace CoreCodeCamp.Data
     {
         public CampProfile()
         {
-            this.CreateMap<Camp, CampModel>();
-            this.CreateMap<CampModel, Camp>();
-                //.ForMember(c => c.VenueName, o => o.MapFrom(m => m.Location.VenueName))
-                //.ForMember(c => c.Address1, o => o.MapFrom(m => m.Location.Address1))
-                //.ForMember(c => c.Address2, o => o.MapFrom(m => m.Location.Address2))
-                //.ForMember(c => c.Address3, o => o.MapFrom(m => m.Location.Address3))
-                //.ForMember(c => c.CityTown, o => o.MapFrom(m => m.Location.CityTown))
-                //.ForMember(c => c.StateProvince, o => o.MapFrom(m => m.Location.StateProvince))
-                //.ForMember(c => c.PostalCode, o => o.MapFrom(m => m.Location.PostalCode))
-                //.ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country));
+            this.CreateMap<Camp, CampModel>()
+                .ReverseMap(); //this.CreateMap<CampModel, Camp>();
+
+            //.ForMember(c => c.VenueName, o => o.MapFrom(m => m.Location.VenueName))
+            //.ForMember(c => c.Address1, o => o.MapFrom(m => m.Location.Address1))
+            //.ForMember(c => c.Address2, o => o.MapFrom(m => m.Location.Address2))
+            //.ForMember(c => c.Address3, o => o.MapFrom(m => m.Location.Address3))
+            //.ForMember(c => c.CityTown, o => o.MapFrom(m => m.Location.CityTown))
+            //.ForMember(c => c.StateProvince, o => o.MapFrom(m => m.Location.StateProvince))
+            //.ForMember(c => c.PostalCode, o => o.MapFrom(m => m.Location.PostalCode))
+            //.ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country));
+
+            this.CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                // TalkModel => Talk (ignore following)
+                .ForMember(t => t.Camp, opt => opt.Ignore())
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
+
+            this.CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
 
         }
     }
